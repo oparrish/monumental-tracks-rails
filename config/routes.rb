ec6d349dd96c4895f9d548d49ffe59cf7e61dc3c
Owen <owen@monumentaltracks.com>
2011-09-16 19:54:44 -0400

    Added routes for log in, sign up, and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-MonumentalTracks::Application.routes.draw do
-  resources :settings
-
-  resources :links
-
-  resources :posts
+MonumentalTracks::Application.routes.draw do		
+	get "log_out" => "sessions#destroy", :as => "log_out"
+	get "log_in" => "sessions#new", :as => "log_in"
+	get "sign_up" => "users#new", :as => "sign_up"
   
-  resources :configs
+  resources :settings
+	resources :links
+	resources :posts
   
   match 'feed/' => 'feed#index'
   
