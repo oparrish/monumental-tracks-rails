f46f19716ac997e8a4035b9472cddf1f34b6c57f
Owen <owen@monumentaltracks.com>
2011-09-19 10:48:56 -0400

    Added a missing )

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('div#read-and-listen > div > a').click(function(event){
-    _gaq.push(['_trackEvent','outbound-read-and-listen',$(this).attr("href")];
+    _gaq.push(['_trackEvent','outbound-read-and-listen',$(this).attr("href")]);
   });
   $('p.episode_download_link > a').click(function(event){
-    _gaq.push(['_trackEvent','outbound-episode-download',$(this).attr("href")];
+    _gaq.push(['_trackEvent','outbound-episode-download',$(this).attr("href")]);
 	});
 });
 
